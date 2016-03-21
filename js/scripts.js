55f1ef487d9019a3cd3fed8aaac8e20bc8b2e4f6
Andrew Russo and Patrick Wells <patrick@>
2016-03-21 13:54:03 -0700

    Made changes to scripts.js file.

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+var add = function(number1,number2) {
+  return number1 + number2;
+}
+
+
+
+var subtract = function(number1,number2) {
+  return number1 - number2;
+}
+
+
+
+
+var multiply = function(number1,number2) {
+  return number1 * number2;
+}
+
+
+
+
+var divide = function(number1,number2) {
+  return number1 / number2;
+}
+
+
+var number1 = parseInt(prompt("Give me a number: "));
+var number2 = parseInt(prompt("Give me another number: "));
+var result = divide(number1,number2)
+alert(result);
